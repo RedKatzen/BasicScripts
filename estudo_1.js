61d266558f1fb062905a28c0b22b81285389dd5a
RedKatzen <gabrieldeavilaf@gmail.com>
2021-09-04 12:07:15 -0300

    First Tests

---
+++ estudo_1.js
@@ -0,0 +1,15 @@
+var datas = 
+    [
+        2000, 
+        1990, 
+        1980, 
+        1970
+    ];
+var conjunto = 
+        [
+            datas,
+            dias = [1, 2, 3], 
+            nomes = ["Gabriel", "Felipe"]
+        ];
+
+console.log("Os nomes são " + conjunto[2][0] + " e " + conjunto[2][1]);        
---
+++ estudo_function.js
@@ -0,0 +1,10 @@
+var number1 = 20;
+var number2 = 30;
+
+function simplesSoma(num1, num2){
+    var soma = num1 + num2;
+    return soma;
+}
+
+let resultado = simplesSoma(number1, number2);
+console.log("Este é o resultado da soma: " + resultado);
